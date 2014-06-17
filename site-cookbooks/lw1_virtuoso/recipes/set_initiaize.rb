4628f58ae299f13e28bb221767f9e8515b1b336c
sawanoboly <sawanoboriyu@higanworks.com>
2014-06-17 23:08:18 +0900

    use cookbook_file for cron entry @reboot

--- site-cookbooks/lw1_virtuoso/recipes/set_initiaize.rb
+++ site-cookbooks/lw1_virtuoso/recipes/set_initiaize.rb
@@ -13,7 +13,7 @@ template '/opt/lw1/initial.rb' do
   source 'initial.rb.erb'
 end
 
-file '/etc/cron.d/first_boot' do
+cookbook_file '/etc/cron.d/first_boot' do
   action :create
-  content '@reboot root /usr/bin/chef-apply /opt/lw1/initial.rb > /dev/null'
+  source 'first_boot'
 end
